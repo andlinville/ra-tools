86cc5fa0e1f74d55764e41be4af7b863e11f5f95
unknown <linville_a@dc5277e.economists-inc.com>
2013-11-19 10:17:49 -0500

    initial commit

---
+++ R/POTColGetters.R
@@ -0,0 +1,23 @@
+GetCont <- function(data) {  
+  return(data$cont)
+}
+
+GetCumCont <- function(data) {
+  return(data$cumCont)
+}
+
+GetShare <- function(data) {
+  return(data$share)
+}
+
+GetCumShare <- function(data) {
+  return(data$cumShare)
+}
+
+GetOtherHosps <- function(data) {
+  return(data$otherHosps)
+}
+
+GetTotal <- function(data) {
+  return(data$allHosps)
+}
---
+++ R/POTKeys.R
@@ -0,0 +1,7 @@
+GenerateHospNameKey(dischargeRaw) {
+  
+}
+
+GenerateOverlapKey(dischargeRaw) {
+  
+}
