8057524242619dfb07e7a75c930d7d6f13f53f2e
andlinville <andlinville@gmail.com>
2013-11-20 13:02:10 -0500

    add payer key function

--- R/POTKeys.R
+++ R/POTKeys.R
@@ -1,7 +1,30 @@
-GenerateHospNameKey(dischargeRaw) {
+GenHospNameKey <- function(dischargeRaw) {
   
 }
 
-GenerateOverlapKey(dischargeRaw) {
+GenOverlapKey <- function(dischargeRaw) {
   
+}
+
+GenPayerKey <- function(moltenDischarge, commercialPayers) {
+  #
+  # Returns a key of commercial payers, to be used as input for the
+  # CastDischarge function
+  #
+  # Args:
+  #   moltenDischarge: the molten discharge data, output of MeltDischarge
+  #   commercialPayers: a vector commercial payers, specified by the payer_d
+  #     field
+  #
+  # Returns:
+  #   a data frame containing the payer descriptions and a dummy flagging
+  #   commercial payers
+  
+  uniquePayers <- unique(moltenDischarge['payer'])
+  commercialDummy <- lapply(uniquePayers, function(x){
+                              as.numeric(x %in% commercialPayers)
+                            })
+  names(commercialDummy) <- c('commercial')
+  result <- data.frame(uniquePayers, commercialDummy)
+  return(result)
 }
